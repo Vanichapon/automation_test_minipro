cecaa31ec4f35436db14c9ab33dfb36c23b97163
Vanichapon <vanichapon.bo@rmuti.ac.th>
2026-03-13 22:05:01 +0700

    first commit

---
+++ tests/RMUTI_Tournament.spec.js
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('RMUTI Tournament System', async ({ page }) => {
+    await page.goto('https://computer.surin.rmuti.ac.th/certificate/project/index.php');
+   
+});
